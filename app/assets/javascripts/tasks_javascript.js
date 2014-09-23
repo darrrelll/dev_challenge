599971fb5d044e7658866bdc3ca8239d21a88474
Darrrelll <sommerlatt.darrell.com>
2014-09-22 19:27:55 -0700

    Tasks even when completed are shown. Style the completed tasks such that it is instantly evident that they are completed.

--- app/assets/javascripts/tasks_javascript.js
+++ app/assets/javascripts/tasks_javascript.js
@@ -1,1 +1,8 @@
 // You can use CoffeScript in tasks.js.coffee if you'd rather.
+
+// Adding styling for quick javascript change
+function click_complete(id)
+{
+    var row2Change = document.getElementsByTagName("tr")[id];
+    $(row2Change).children().toggleClass("complete-color");
+}
